e9ff05860bbbbfc33e3febdcd806cd0c5bede3be
Tu Nombre <tu_correo@ejemplo.com>
2025-01-20 11:53:28 -0500

    Se modifico el modulo reportes

--- src/pages/Reports/index.js
+++ src/pages/Reports/index.js
@@ -1,7 +1,7 @@
 // src/pages/Places/Places.jsx
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import { collection, getDocs } from "firebase/firestore";
+import { collection, getDocs, query, where } from "firebase/firestore";
 import { db } from "../../firebase";
 import { NavBar } from "../../components/NavBar";
 import { Footer } from "../../components/Footer";
@@ -15,32 +15,71 @@ ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Tooltip,
 
 export function Reports() {
   const [reports, setReports] = useState([]);
+  const [reportsGPT, setReportsGPT] = useState([]);
   const [reportsGemini, setReportsGemini] = useState([]);
+  const [reportsClaude, setReportsClaude] = useState([]);
 
-  const getReportsOperaVariables = async () => {
-    const reportsCollection = collection(db, "reportsOperaVariables");
-    const data = await getDocs(reportsCollection);
-    setReports(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+  const getReportsGPT = async () => {
+    try {
+      const reportsCollection = collection(db, "reportsModels");
+      const Query = query(reportsCollection, where("usedModel", "==", "gpt-3.5-turbo"));
+      const data = await getDocs(Query);
+
+      console.log("Documentos obtenidos:", data.docs);  // Verifica si hay documentos
+      if (data.docs.length > 0) {
+        setReportsGPT(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+      } else {
+        console.log("No se encontraron reportes para 'gpt-3.5-turbo'");
+      }
+    } catch (error) {
+      console.error("Error al obtener los reportes de Gemini:", error);
+    }
   };
-
   const getReportsGenimi = async () => {
-    const reportsCollection = collection(db, "reportsGemini");
-    const data = await getDocs(reportsCollection);
-    setReportsGemini(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
-  };
+    try {
+      const reportsCollection = collection(db, "reportsModels");
+      const geminiQuery = query(reportsCollection, where("usedModel", "==", "gemini-flash"));
+      const data = await getDocs(geminiQuery);
 
+      console.log("Documentos obtenidos:", data.docs);  // Verifica si hay documentos
+      if (data.docs.length > 0) {
+        setReportsGemini(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+      } else {
+        console.log("No se encontraron reportes para 'gemini-flash'");
+      }
+    } catch (error) {
+      console.error("Error al obtener los reportes de Gemini:", error);
+    }
+  };
+  const getReportsClaude = async () => {
+    try {
+      const reportsCollection = collection(db, "reportsModels");
+      const geminiQuery = query(reportsCollection, where("usedModel", "==", "claude-3.5-sonnet-20241022"));
+      const data = await getDocs(geminiQuery);
+
+      console.log("Documentos obtenidos:", data.docs);  // Verifica si hay documentos
+      if (data.docs.length > 0) {
+        setReportsClaude(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+      } else {
+        console.log("No se encontraron reportes para 'claude-3.5-sonnet-20241022'");
+      }
+    } catch (error) {
+      console.error("Error al obtener los reportes de Gemini:", error);
+    }
+  };
   useEffect(() => {
-    getReportsOperaVariables();
+    getReportsGPT();
     getReportsGenimi();
+    getReportsClaude();
   }, []);
 
   // Preparar datos para el gráfico de dispersión (OpenAI)
   const LineData1 = {
-    labels: reports.map((report) => report.outputCharacters), // Usar Input Characters como etiquetas
+    labels: reportsGPT.map((report) => report.inputCharacters), // Usar Input Characters como etiquetas
     datasets: [
       {
-        label: 'Output Characters',
-        data: reports.map((report) => ({
+        label: 'Input Characters',
+        data: reportsGPT.map((report) => ({
           x: report.inputCharacters,
           y: report.responseTime, // Tiempo como Y
         })),
@@ -51,19 +90,35 @@ export function Reports() {
       }
     ],
   };
-
-
+  // Preparar datos para el gráfico de dispersión (OpenAI)
+  const LineData2 = {
+    labels: reportsClaude.map((report) => report.inputCharacters), // Usar Input Characters como etiquetas
+    datasets: [
+      {
+        label: 'Input Characters',
+        data: reportsClaude.map((report) => ({
+          x: report.inputCharacters,
+          y: report.responseTime, // Tiempo como Y
+        })),
+        fill: false,
+        backgroundColor: 'rgba(75, 192, 192, 0.6)', // Color de fondo para Input
+        borderColor: 'rgba(75, 192, 192, 1)', // Color sólido para Input
+        pointRadius: 5, // Radio de los puntos
+      }
+    ],
+  };
   // Preparar datos para el gráfico de líneas (Gemini)
   const lineData = {
-    labels: reportsGemini.map((report) => report.outputCharacters), // Usar Input Characters como etiquetas
+    labels: reportsGemini.map((report) => report.inputCharacters), // Usar Input Characters como etiquetas
     datasets: [{
-      label: 'Output Characters',
+      label: 'Input Characters',
       data: reportsGemini.map((report) => report.responseTime), // Respuesta de tiempo
       fill: false,
       backgroundColor: 'rgba(255, 99, 132, 0.6)',
       borderColor: 'rgba(255, 99, 132, 1)',
     },],
   };
+
   const options = {
     scales: {
       x: {
@@ -80,6 +135,56 @@ export function Reports() {
       },
     },
   };
+
+  const multiAxisLineData = {
+    labels: reportsGPT.map((report, index) => report.inputCharacters),  // Usamos los inputCharacters como etiquetas
+    datasets: [
+      {
+        label: 'GPT-3.5-turbo',
+        data: reportsGPT.map((report) => report.responseTime), // Tiempo de respuesta para GPT-3.5
+        fill: false,
+        borderColor: 'rgba(75, 192, 192, 1)',
+        backgroundColor: 'rgba(75, 192, 192, 0.6)',
+        yAxisID: 'y',
+      },
+      {
+        label: 'Gemini-flash',
+        data: reportsGemini.map((report) => report.responseTime), // Tiempo de respuesta para Gemini
+        fill: false,
+        borderColor: 'rgba(255, 99, 132, 1)',
+        backgroundColor: 'rgba(255, 99, 132, 0.6)',
+        yAxisID: 'y',
+      },
+      {
+        label: 'Claude',
+        data: reportsClaude.map((report) => report.responseTime), // Tiempo de respuesta para Claude
+        fill: false,
+        borderColor: 'rgba(54, 162, 235, 1)',
+        backgroundColor: 'rgba(54, 162, 235, 0.6)',
+        yAxisID: 'y',
+      },
+    ],
+  };
+
+  const multiAxisOptions = {
+    responsive: true,
+    scales: {
+      x: {
+        title: {
+          display: true,
+          text: 'Input Characters',
+        },
+      },
+      y: {
+        title: {
+          display: true,
+          text: 'Response Time (Seconds)',
+        },
+        beginAtZero: true,
+      },
+    },
+  };
+
   return (
     <div className="flex flex-col min-h-screen bg-white dark:bg-slate-900 text-black dark:text-white">
       <NavBar />
@@ -90,7 +195,7 @@ export function Reports() {
           <div className="bg-white dark:bg-slate-900 shadow-lg  p-2 md:p-4 rounded-lg font-sans">
             {/* Cabecera */}
             <div className="flex justify-between items-center mb-4">
-              <h1 className="text-lg md:text-xl font-bold">Gpt-3.5-turbo Vs Gemini-1.5-flash</h1>
+              <h1 className="text-lg md:text-xl font-bold">Compartivo de modelos genertivos Claude || Gemini || OpenAI</h1>
               <Link to="/reports">
                 <button className="button text-white text-sm md:text-base px-3 py-2 rounded hover:bg-blue-700 transition flex items-center">
                   <FaRedo />
@@ -100,7 +205,10 @@ export function Reports() {
             </div>
 
             <hr className="my-2 md:my-4 border-gray-300 dark:border-gray-600" />
-
+            <div className="bg-white dark:bg-slate-900 shadow-lg p-2 md:p-4 rounded-lg font-sans">
+              <h1 className="text-lg md:text-xl font-bold">Comparativa de Modelos</h1>
+              <Line data={multiAxisLineData} options={multiAxisOptions} />
+            </div>
             <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
               {/* Gráfico de dispersión (OpenAI) */}
               <div className="overflow-x-auto">
@@ -113,13 +221,19 @@ export function Reports() {
                 <h1 className="text-lg md:text-xl font-bold">API Gemini-1.5-flash</h1>
                 <Line data={lineData} options={options} />
               </div>
+
+              {/* Gráfico de líneas (Gemini) */}
+              <div className="overflow-x-auto">
+                <h1 className="text-lg md:text-xl font-bold">API Claude</h1>
+                <Line data={LineData2} options={options} />
+              </div>
             </div>
 
             <hr className="my-2 md:my-4 border-gray-300 dark:border-gray-600" />
 
             {/* Tabla de resultados */}
             <div className="overflow-x-auto">
-              <h1 className="text-lg md:text-xl font-bold">Tiempo de respuesta de API de Gpt-3.5-turbo</h1>
+              <h1 className="text-lg md:text-xl font-bold">Tiempo de respuesta de API de Gpt-3.5-turbo de OpenAI</h1>
               <table className="min-w-full table-auto text-xs md:text-sm">
                 <thead className="bg-gray-50 dark:bg-slate-800 text-gray-700 dark:text-gray-300 uppercase">
                   <tr>
@@ -130,8 +244,8 @@ export function Reports() {
                   </tr>
                 </thead>
                 <tbody>
-                  {Array.isArray(reports) && reports.length > 0 ? (
-                    reports.map((report, index) => (
+                  {Array.isArray(reportsGPT) && reportsGPT.length > 0 ? (
+                    reportsGPT.map((report, index) => (
                       <tr key={report.id} className="bg-white dark:bg-slate-900 border-b hover:bg-gray-50 dark:hover:bg-gray-600">
                         <td className="px-2 py-1 md:px-4 md:py-2">{index + 1}</td>
                         <td className="px-2 py-1 md:px-4 md:py-2">{report.inputCharacters}</td>
@@ -151,7 +265,7 @@ export function Reports() {
             </div>
 
             <div className="overflow-x-auto">
-              <h1 className="text-lg md:text-xl font-bold">Tiempo de respuesta de API Gemini-1.5-flash</h1>
+              <h1 className="text-lg md:text-xl font-bold">Tiempo de respuesta de API Gemini-1.5-flash  de Google</h1>
               <table className="min-w-full table-auto text-xs md:text-sm">
                 <thead className="bg-gray-50 dark:bg-slate-800 text-gray-700 dark:text-gray-300 uppercase">
                   <tr>
@@ -164,6 +278,38 @@ export function Reports() {
                 <tbody>
                   {Array.isArray(reportsGemini) && reportsGemini.length > 0 ? (
                     reportsGemini.map((report, index) => (
+                      <tr key={report.id} className="bg-white dark:bg-slate-900 border-b hover:bg-gray-50 dark:hover:bg-gray-600">
+                        <td className="px-2 py-1 md:px-4 md:py-2">{index + 1}</td>
+                        <td className="px-2 py-1 md:px-4 md:py-2">{report.inputCharacters}</td>
+                        <td className="px-2 py-1 md:px-4 md:py-2">{report.outputCharacters}</td>
+                        <td className="px-2 py-1 md:px-4 md:py-2">{report.responseTime} Seg.</td>
+                      </tr>
+                    ))
+                  ) : (
+                    <tr>
+                      <td colSpan="6" className="text-center py-4 text-gray-500 dark:text-gray-400">
+                        No hay datos disponibles.
+                      </td>
+                    </tr>
+                  )}
+                </tbody>
+              </table>
+            </div>
+
+            <div className="overflow-x-auto">
+              <h1 className="text-lg md:text-xl font-bold">Tiempo de respuesta de API Claude de Anthropic</h1>
+              <table className="min-w-full table-auto text-xs md:text-sm">
+                <thead className="bg-gray-50 dark:bg-slate-800 text-gray-700 dark:text-gray-300 uppercase">
+                  <tr>
+                    <th className="px-2 py-1 md:px-4 md:py-2">#</th>
+                    <th className="px-2 py-1 md:px-4 md:py-2">Input Characters</th>
+                    <th className="px-2 py-1 md:px-4 md:py-2">Output Characters</th>
+                    <th className="px-2 py-1 md:px-4 md:py-2">Time</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {Array.isArray(reportsClaude) && reportsClaude.length > 0 ? (
+                    reportsClaude.map((report, index) => (
                       <tr key={report.id} className="bg-white dark:bg-slate-900 border-b hover:bg-gray-50 dark:hover:bg-gray-600">
                         <td className="px-2 py-1 md:px-4 md:py-2">{index + 1}</td>
                         <td className="px-2 py-1 md:px-4 md:py-2">{report.inputCharacters}</td>
